7d18891c14e480b949ad5e7ace2585bbba1aab04
Leeann D. Louis <67008393+leeannlouis@users.noreply.github.com>
2020-09-27 10:20:46 -0400

    Modified to work with osteocyte.db SQL database
    Loads the osteocyte.db database and inserts new data directly into the database. Also outputs a copy of the relevant table.

--- osteocyteLacunarProcessing.m
+++ osteocyteLacunarProcessing.m
@@ -1,16 +1,17 @@
 %% Take .czi(s) and save tif stack(s) to use for lacunar analysis
-% This script takes in the directory where the processing data is held, and
-% .czi file(s). It loads those volumes, rotates and crops them as directed
-% by the user, and outputs them into the directory. It also outputs a new
-% table with the processing summary, including rotations applied, image 
-% size, crop locations, and input / output file names.
+% This script takes in the .czi file(s), which are 2D images or 3D stacks
+% taken by Zeiss-branded microscope. Here we used the LSM800, a laser
+% scanning confocal microscope. Images are of fluorescent stained cortical
+% bone. Script loads those volumes, rotates and crops them as directed
+% by the user, and outputs them as a .tif stack  into the given output 
+% directory. It also adds this processing data (rotation, cropping
+% locations, image size, processing date, etc.) to the database.
 
-% Must have Bio-Formats installed. Put it in the toolbox folder and then
-% use the pathtool to ensure it is included.
-% https://docs.openmicroscopy.org/bio-formats/6.1.0/developers/matlab-dev.html
+% Must have Bio-Formats MATLAB Toolbox installed. Download bfmatlab.zip
+% from the page below and add to the toolbox using the pathtool
+% https://www.openmicroscopy.org/bio-formats/downloads/
 
 % Setup
-% cd(); % Change to the directory where your custom functions are held
 clear; clc; close all;
 % set(0,'DefaultFigureWindowStyle','docked') % Dock figures if preferred
 
@@ -18,17 +19,16 @@ clear; clc; close all;
 thickness = 30;     % in um. Crop all data to this thickness. 
 
 % Get the output data directory
-oDir = uigetdir('', 'Where is the table / where to output data?');
+oDir = uigetdir('', 'Analysis folder to output .csv and .tif files');
 
-% Get the processing table data
-[maxVers, pFile] = getMaxFileVers(oDir, 'lacunarDensityProcessing');
-pTable = readtable(fullfile(oDir, pFile), 'ReadVariableNames', true);
-pTableNew = pTable;     % Create a copy of the tables for concatenation
+% Connect to the osteocyte.db database
+%[dbfile, dbpath] = uigetfile('\*.db', 'Choose the .db file');
+dbpath = 'C:\Users\Leeann\OneDrive - CUNY\project_osteocyte\analyses';
+dbfile = 'osteocyte.db';
+mksqlite('open', fullfile(dbpath, dbfile));
 
-% Get the date in the proper format
+% Get today string for future use
 todayStr = getTodayStr();
-
-%% Load czi files, process (rotate and crop), save volume and metadata
 
 % Get the .czi file(s) to analyze
 [cfiles, cpath] = uigetfile('.czi', 'Choose the .czi file(s)', 'MultiSelect', 'on');
@@ -38,14 +38,17 @@ if ischar(cfiles)
     cfiles = {cfiles};  
 end
 
+% Step through all the data
 for cIdx = 1:size(cfiles, 2)
    
     % Get the file name
     cfile = cfiles{cIdx};
     
     % Check if the file has been evaluated previously
+    priorData = mksqlite(['SELECT * FROM processing WHERE ' ...
+        'InputFileName = "', cfile, '"']);
     redo = 'NA';
-    if ismember(cfile, pTable.InputFileName)
+    if ~isempty(priorData)
         
         % Ask user if they want to redo the processing
         redo = input(['Data already exists for ', cfile, '. Redo? Y/N '], 's');
@@ -67,20 +70,18 @@ for cIdx = 1:size(cfiles, 2)
         [~, czi] = evalc('bfopen([cpath, cfile])');
         fprintf('opened.\n');
     catch
-        fprintf('cannot open, not a Bioformats file.\n');
+        fprintf('not a Bioformats file or Bioformats not installed.\n');
     end
 
     % Get key file metadata
     metadata = czi{1, 4};
     width = metadata.getPixelsSizeX(0).getValue();
     height = metadata.getPixelsSizeY(0).getValue();
-    allSlices = metadata.getPixelsSizeZ(0).getValue();
-    xstep = str2double(metadata.getPixelsPhysicalSizeX(0).value(ome.units.UNITS.MICROMETER));
-    ystep = str2double(metadata.getPixelsPhysicalSizeY(0).value(ome.units.UNITS.MICROMETER));
     zstep = str2double(metadata.getPixelsPhysicalSizeZ(0).value(ome.units.UNITS.MICROMETER));
-
-    % Get the desired number of slices based on the hard-coded thickness (could
-    % also use getCropZ to choose manual cropping)
+    
+    % Get the desired number of slices based on the hard-coded thickness 
+    % (could also use getCropZ to choose manual cropping) and the file's
+    % pixel size in the z-axis.
     numSlices = floor(thickness / zstep); 
 
     % Get the data
@@ -106,8 +107,9 @@ for cIdx = 1:size(cfiles, 2)
     % Rotate the volume
     volRot = imrotate(volume, rotation);
 
-    % Get the cropping region in XY
-    [xmin, xmax, ymin, ymax] = getCropXY(volRot);
+    % Get the cropping region in XY. Use an exclusive cropping region to
+    % ensure that no external parts of 
+    [xmin, xmax, ymin, ymax] = getCropXY(volRot, 'exclusive');
     volCrop = volRot(ymin:ymax, xmin:xmax, :);
 
     % Save the new volume in a multi-page tif with the same name as the
@@ -115,33 +117,32 @@ for cIdx = 1:size(cfiles, 2)
     [~, pFileName, ~] = fileparts(cfile);
     pFileName = [pFileName, '_proc', todayStr, '.tif'];
     fprintf(['Saving data with name ', char(pFileName), '... '])
-    writeMultipageTif(volCrop, fullfile(oDir, pFileName));
+    %writeMultipageTif(volCrop, fullfile(oDir, pFileName));
     fprintf('done. \n')
 
-    % Collect metadata
-    pTableAdd = array2table(cell(1, size(pTable, 2)), ...
-        'VariableNames', pTable.Properties.VariableNames); % Make a clean row
+    % Collect the data to add and get it in the right format for use in a
+    % SQL query. That is, TEXT outputs have double quotes around them,
+    % numbers are converted to strings for use in the query, and all values
+    % are separated by commas
     dataDirParts = regexp(cpath, '\', 'split'); % Get the input data directory
-    pTableAdd.DataDirectory = dataDirParts{end-1};
-    pTableAdd.InputFileName = cfile;
-    pTableAdd.OutputFileName = pFileName;
-    pTableAdd.DateProcessed = todayStr;
-    pTableAdd.RotationPerformed_Deg = rotation;
-    pTableAdd.IsRedo = redo;
-    pTableAdd.Width_Pixels = size(volCrop, 2);
-    pTableAdd.Height_Pixels = size(volCrop, 1);
-    pTableAdd.Depth_Pixels = size(volCrop, 3);
-    pTableAdd.XResolution_um = xstep;
-    pTableAdd.YResolution_um = ystep;
-    pTableAdd.ZResolution_um = zstep; 
-    pTableAdd.CropX1 = xmin;        pTableAdd.CropX2 = xmax;
-    pTableAdd.CropY1 = ymin;        pTableAdd.CropY2 = ymax;
-    pTableAdd.CropZ1 = 1;           pTableAdd.CropZ2 = numSlices;
-
-    % Append the new data
-    pTableNew = vertcat(pTableNew, pTableAdd);
+    values_to_add = createSqlValueList({dataDirParts{end-1}, cfile, pFileName, ...
+        todayStr, redo, rotation, size(volCrop, 2), size(volCrop, 1), ...
+        size(volCrop, 3), xmin, xmax, ymin, ymax, 1, numSlices});
+    
+    % Add the data to the database
+    mksqlite(['INSERT INTO processing(DataDirectory, InputFileName, ', ...
+        'OutputFileName, DateProcessed, IsRedo, RotationPerformed_Deg, ', ...
+        'Width_Pixels, Height_Pixels, Depth_Pixels, CropX1, CropX2, ', ...
+        'CropY1, CropY2, CropZ1, CropZ2) VALUES (', values_to_add, ')']);
 
 end
 
-% Save the updated metadata file
-writeNewTable(pTable, pTableNew, oDir, 'lacunarDensityProcessing', maxVers + 1);
+% Get a copy of the new processing datasheet that you appended here
+% using a SQL command. It comes out as a structure, convert to a table.
+newTable = struct2table(mksqlite('SELECT * FROM processing'));
+fileNameOut = ['processing-', todayStr, '.csv'];
+writetable(newTable, fullfile(oDir, fileNameOut));
+fprintf(['New file created: ', fileNameOut, '\n']);
+
+% Close the database
+mksqlite('close');
